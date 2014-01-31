3c9043a0552e23d4b9c0413df5662ae5f8c81bd7
ashwinh <ashwinh@cybage.com>
2014-01-31 12:20:43 +0530

    Signed-off-by: ashwinh <ashwinh@cybage.com>

---
+++ Examples/Example7/script.js
@@ -0,0 +1,48 @@
+/***
+ * 1. Understanding jQuery's Shorthand Methods - $.getJSON()
+ */
+
+$(function() {
+	/***
+	 * Load JSON-encoded data from the server using a GET HTTP request.
+	 */
+	$.getJSON("index.json")
+		.done(function(data) {
+			console.log('Getting `JSON` file');
+			console.log('--Success--');
+			/***
+			 * console what was returned
+			 */
+			console.log('Data: ');
+			console.log(data);
+			console.log('Type of data is: ' + typeof data);
+
+			console.log('Total number of employees are: ' + data.employees.length);
+
+			/***
+			 * 1st Way: used for loop over object.
+			 */
+			console.log("Using FOR loop: ");
+			for (var employee = 0; employee < data.employees.length; employee++) {
+				console.log(data.employees[employee].id);
+				// console.log(data.employees[employee].firstname);
+				// console.log(data.employees[employee].lastname);
+			}
+
+			/***
+			 * 2nd Way: used for-in loop over object.
+			 */
+			console.log("Using FOR-IN loop: ");
+			for (var employee in data.employees) {
+				console.log(data.employees[employee].id);
+				// console.log(data.employees[employee].firstname);
+				// console.log(data.employees[employee].lastname);
+			}
+		})
+		.fail(function() {
+			console.log('--Error--');
+		})
+		.always(function() {
+			console.log('--Finished--');
+		});
+});
