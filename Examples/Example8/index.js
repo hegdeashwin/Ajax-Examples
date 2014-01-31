cd21def20a01e4a6314f2d1e3a8a5f7d9bed433a
ashwinh <ashwinh@cybage.com>
2014-01-31 12:30:48 +0530

    Signed-off-by: ashwinh <ashwinh@cybage.com>

---
+++ Examples/Example8/index.js
@@ -0,0 +1,18 @@
+function prepareData() {
+	var studentObj = {
+		"students": [{
+			"id": 101,
+			"firstname": "Ashwin",
+			"lastname": "Hegde"
+		}, {
+			"id": 102,
+			"firstname": "Vinayak",
+			"lastname": "Patil"
+		}, {
+			"id": 103,
+			"firstname": "Jerin",
+			"lastname": "John"
+		}]
+	};
+	return studentObj;
+};
---
+++ Examples/Example8/script.js
@@ -0,0 +1,31 @@
+/***
+ * 1. Understanding jQuery's Shorthand Methods - $.getScript()
+ */
+
+$(function() {
+	/***
+	 *  Load a JavaScript file from the server using a GET HTTP request, then execute it.
+	 */
+	$.getScript("index.js")
+		.done(function() {
+			console.log('--Success--');
+			var data = prepareData();
+			console.log('Total number of employees are: ' + data.students.length);
+
+			/***
+			 * Used for-in loop over object.
+			 */
+			console.log("Using FOR-IN loop: ");
+			for (var student in data.students) {
+				console.log(data.students[student].id);
+				// console.log(data.students[student].firstname);
+				// console.log(data.students[student].lastname);
+			}
+		})
+		.fail(function() {
+			console.log('--Error--');
+		})
+		.always(function() {
+			console.log('--Finished--');
+		});
+});
